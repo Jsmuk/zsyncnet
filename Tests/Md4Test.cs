8e21f52829175b4241fe404c477d4c362cf8c936
Keithen <Keithen.Neu@gmail.com>
2021-11-08 01:11:43 +0100

    speed up md4 a little

---
+++ Tests/Md4Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using zsyncnet;
+
+namespace Tests
+{
+    public class Md4Test
+    {
+        [Test]
+        public void TestPerformance()
+        {
+            var rounds = 100000;
+            var data = new byte[2048];
+            new Random().NextBytes(data);
+
+            var start = DateTime.Now;
+            for (var i = 0; i < rounds; i++)
+            {
+                ZsyncUtil.Md4Hash(data);
+            }
+
+            var duration = DateTime.Now - start;
+            TestContext.Out.WriteLine(duration.TotalSeconds.ToString(CultureInfo.InvariantCulture) + "sec");
+            // 0.85sec on my pc, when ran alone.
+        }
+
+        [Test]
+        public void TestCorrectness1()
+        {
+            var data = new byte[2048];
+            new Random(123).NextBytes(data);
+
+            var hash = ZsyncUtil.ByteToHex(ZsyncUtil.Md4Hash(data));
+
+            Assert.AreEqual("e44ba21ef5d141f3d5c97d34c9ac0542", hash);
+        }
+
+        [Test]
+        public void TestCorrectness2()
+        {
+            var data = new byte[2048];
+            new Random(456).NextBytes(data);
+
+            var hash = ZsyncUtil.ByteToHex(ZsyncUtil.Md4Hash(data));
+
+            Assert.AreEqual("2651e83ccad13c1054a0942844deb4e4", hash);
+        }
+
+        [Test]
+        public void TestCorrectness3()
+        {
+            var data = new byte[2048];
+            new Random(789).NextBytes(data);
+
+            var hash = ZsyncUtil.ByteToHex(ZsyncUtil.Md4Hash(data));
+
+            Assert.AreEqual("812481225a4cd3b722556ec916aae9cd", hash);
+        }
+    }
+}
--- zsyncnet/ZsyncUtil.cs
+++ zsyncnet/ZsyncUtil.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -46,42 +45,48 @@ namespace zsyncnet
 
         }
 
+        private static readonly uint[] Y1 = { 0, 4, 8, 12, 0, 1, 2, 3, 3, 7, 11, 19, 0 };
+        private static readonly uint[] Y2 = { 0, 1, 2, 3, 0, 4, 8, 12, 3, 5, 9, 13, 0x5a827999 };
+        private static readonly uint[] Y3 = { 0, 2, 1, 3, 0, 8, 4, 12, 3, 9, 11, 15, 0x6ed9eba1 };
+
         public static byte[] Md4Hash(byte[] input)
         {
-            List<Byte> bytes = new List<byte>(input);
-            // get padded uints from bytes
-            uint bitCount = (uint) (bytes.Count) * 8;
-            bytes.Add(128);
-            while (bytes.Count % 64 != 56) bytes.Add(0);
-            var uints = new List<uint>();
-            for (int i = 0; i + 3 < bytes.Count; i += 4)
-                uints.Add(bytes[i] | (uint) bytes[i + 1] << 8 | (uint) bytes[i + 2] << 16 | (uint) bytes[i + 3] << 24);
-            uints.Add(bitCount);
-            uints.Add(0);
+            var size = input.Length + 1;
+            while (size % 64 != 56) size++; // TODO: go back to school
+            var bytes = new byte[size];
+            Array.Fill<byte>(bytes, 0);
+            input.CopyTo(bytes, 0);
+            bytes[input.Length] = 128;
+
+            uint bitCount = (uint) (input.Length) * 8;
+            var uints = new uint[bytes.Length / 4 + 2];
+            for (int i = 0; i + 3 < bytes.Length; i += 4)
+                uints[i / 4] = bytes[i] | (uint)bytes[i + 1] << 8 | (uint)bytes[i + 2] << 16 | (uint)bytes[i + 3] << 24;
+            uints[^2] = bitCount;
+            uints[^1] = 0;
 
             // run rounds
             uint a = 0x67452301, b = 0xefcdab89, c = 0x98badcfe, d = 0x10325476;
             Func<uint, uint, uint> rol = (x, y) => x << (int) y | x >> 32 - (int) y;
-            for (int q = 0; q + 15 < uints.Count; q += 16)
+            for (int q = 0; q + 15 < uints.Length; q += 16)
             {
-                var chunk = uints.GetRange(q, 16);
                 uint aa = a, bb = b, cc = c, dd = d;
 
                 void Round(Func<uint, uint, uint, uint> f, uint[] y)
                 {
-                    foreach (uint i in new[] { y[0], y[1], y[2], y[3] })
+                    for (int j = 0; j < 4; j++)
                     {
-                        a = rol(a + f(b, c, d) + chunk[(int)(i + y[4])] + y[12], y[8]);
-                        d = rol(d + f(a, b, c) + chunk[(int)(i + y[5])] + y[12], y[9]);
-                        c = rol(c + f(d, a, b) + chunk[(int)(i + y[6])] + y[12], y[10]);
-                        b = rol(b + f(c, d, a) + chunk[(int)(i + y[7])] + y[12], y[11]);
+                        var i = y[j];
+                        a = rol(a + f(b, c, d) + uints[q + (int)(i + y[4])] + y[12], y[8]);
+                        d = rol(d + f(a, b, c) + uints[q + (int)(i + y[5])] + y[12], y[9]);
+                        c = rol(c + f(d, a, b) + uints[q + (int)(i + y[6])] + y[12], y[10]);
+                        b = rol(b + f(c, d, a) + uints[q + (int)(i + y[7])] + y[12], y[11]);
                     }
                 }
 
-                Round((x, y, z) => (x & y) | (~x & z), new uint[] {0, 4, 8, 12, 0, 1, 2, 3, 3, 7, 11, 19, 0});
-                Round((x, y, z) => (x & y) | (x & z) | (y & z),
-                    new uint[] {0, 1, 2, 3, 0, 4, 8, 12, 3, 5, 9, 13, 0x5a827999});
-                Round((x, y, z) => x ^ y ^ z, new uint[] {0, 2, 1, 3, 0, 8, 4, 12, 3, 9, 11, 15, 0x6ed9eba1});
+                Round((x, y, z) => (x & y) | (~x & z), Y1);
+                Round((x, y, z) => (x & y) | (x & z) | (y & z), Y2);
+                Round((x, y, z) => x ^ y ^ z, Y3);
                 a += aa;
                 b += bb;
                 c += cc;
