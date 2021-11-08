27fb7c8c018492abccca15d6ed74a92164cdbcaa
Keithen <Keithen.Neu@gmail.com>
2021-11-08 01:12:40 +0100

    add rolling checksum

---
+++ Tests/RollingChecksumTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using zsyncnet;
+using zsyncnet.Internal;
+
+namespace Tests
+{
+    public class RollingChecksumTests
+    {
+        [Test]
+        public void Test()
+        {
+            const int blockSize = 2048;
+            var data = new byte[4096];
+            new Random(123).NextBytes(data);
+            var rSum = RollingChecksum.GetRollingChecksum(data, blockSize);
+
+            var fromRolling = rSum.ToList();
+            var expected = new List<uint>();
+
+            for (int i = 0; i < data.Length - blockSize + 1; i++)
+            {
+                var block = new byte[blockSize];
+                Array.Copy(data, i, block, 0, blockSize);
+                expected.Add(ZsyncUtil.ComputeRsum(block));
+            }
+
+            Assert.AreEqual(expected, fromRolling);
+        }
+
+    }
+}
---
+++ zsyncnet/Internal/RollingChecksum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+// TODO: where should this be placed?
+[assembly: InternalsVisibleTo("Tests")]
+
+namespace zsyncnet.Internal
+{
+    internal static class RollingChecksum
+    {
+        public static IEnumerable<uint> GetRollingChecksum(byte[] array, int blockSize)
+        {
+            ushort a = 0, b = 0;
+            for (int i = 0; i < blockSize; i++)
+            {
+                a += array[i];
+                b += (ushort)((blockSize - i) * array[i]);
+            }
+
+            yield return ToInt(a, b);
+
+            for (int i = 0; i < array.Length - blockSize; i++)
+            {
+                a = (ushort)(a - array[i] + array[i + blockSize]);
+                b = (ushort)(b - blockSize * array[i] + a);
+                yield return ToInt(a, b);
+            }
+        }
+
+        private static uint ToInt(ushort a, ushort b)
+        {
+            // TODO: rsync one is the other way around. not sure what's going on
+            return (uint)(a << 16) | b;
+        }
+    }
+}
